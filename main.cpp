8c9dc7e9c47eba044b9b3610419417c87f50bc0f
kathy18 <katherine.jimenez@ucsp.edu.pe>
2017-03-23 15:49:48 -0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x=2;
+    float y=5;
+    int z=x+y;
+    int a=x-y;
+    int b=x*y;
+    float c=x/y;
+    cout<<"la suma de " <<x<<" y "<<y<<" es "<<z<<endl;
+    cout<<"la resta de " <<x<<" y "<<y<<" es "<<a<<endl;
+    cout<<"la multiplicacion de " <<x<<" y "<<y<<" es "<<b<<endl;
+    cout<<"la division de " <<x<<" y "<<y<<" es "<<c<<endl;
+
+
+    return 0;
+}
